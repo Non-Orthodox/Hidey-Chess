2f78a23745183c5f8781f771d706f507cfebbd39
Blake <bctennis525@gmail.com>
2021-07-05 17:45:38 -0500

    Added basic window

--- main.cpp
+++ main.cpp
@@ -11,5 +11,29 @@ int main(){
     pawn.setCoords(2,3);
     pawn.giveCoords();
     std::cout << x[0] << " " << x[1] << std::endl;
+
+    //Initializing SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "SDL failed to initialize" << SDL_GetError() << std::endl;
+        return 1; //later on use an exception
+    }
+    
+    //Creating Window
+    SDL_Window* window = SDL_CreateWindow("Hidey-Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 500, SDL_WINDOW_RESIZABLE);
+    if (window == NULL) 
+    {
+		std::cerr << "Error: Window could not be created | SDL_Error " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    //Destroying Window
+    int var;
+    std::cin >> var;
+	SDL_DestroyWindow(window);
+	window = NULL;
+	
+	SDL_Quit();
+
     return 0;
 }
