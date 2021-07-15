83bba1431e8d53983f65f1ed624cf5f4df03618b
Blake <bctennis525@gmail.com>
2021-07-15 00:45:48 -0500

    added tile and board classes

--- board.h
+++ board.h
@@ -3,10 +3,44 @@
 
 #include "types.h"
 #include "piece.h"
+#include <vector>
 
 void standardChessBoardInit(piece [][8]);
 void printChessBoard(piece [][8]);
 
+class tile
+{
+    public:
+        piece* p;
+        int color;
+        tile() {}
+        tile(int color)
+        {
+            this->color = color;
+        }
+        tile(piece* piece, int color)
+        {
+            this->p = piece;
+            this->color = color;
+        }
+        ~tile() {}
+};
 
+class board
+{
+    private:
+        int width, height = 8;
+    public:
+        std::vector<piece> pieceList;
+        tile* tiles;
+
+        board() {}
+        board(int width, int height)
+        {
+
+        }
+        ~board() {}
+
+};
 
 #endif
