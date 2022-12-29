38ca6d03fc259b9e2fe9a5f8ba986c937db4788e
Joey Herguth <oitzujoey@gmail.com>
2022-12-28 23:11:06 -0800

    Tidy my files.

--- core/duck-lisp.hpp
+++ core/duck-lisp.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 extern "C" {
 #include "duck-lisp/DuckLib/memory.h"
 #include "duck-lisp/duckLisp.h"
@@ -9,6 +10,9 @@ class DuckLisp {
 	duckLisp_t duckLisp;
 	dl_memoryAllocation_t memoryAllocation;
 public:
-	DuckLisp(const std::size_t hunk_size);
+	DuckLisp(const std::size_t);
 	~DuckLisp();
+	int registerGenerator();
+	int registerCallback();
+	int eval(const std::string);
 };
