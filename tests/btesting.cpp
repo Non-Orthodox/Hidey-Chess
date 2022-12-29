5359dd56c5f21120a795125fad9408aaa380d598
Non-Orthodox <bctennis525@gmail.com>
2022-12-29 13:50:25 -0600

    Moved headers to include file and fixed linking issues

--- tests/btesting.cpp
+++ tests/btesting.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-int main(int argc, char *argv[]){
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main ()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        std::cout << "SDL_Init Error: " << SDL_GetError() << "\n";
+    
+    // if (!(IMG_Init(IMG_INIT_PNG)))
+    //     std::cout << "IMG_Init Error: " << SDL_GetError() << "\n";
+
+
+
     return 0;
 }
